d2462466fba7bc6fb84cb24b705fb442a0fa7338
Sergei Vorfolomeev <vorfo1897@gmail.com>
2023-09-01 21:59:40 +0200

    fix: bug with title font-size

--- components/projects/projects-title.tsx
+++ components/projects/projects-title.tsx
@@ -6,7 +6,7 @@ type PropsType = {
 
 export function ProjectsTitle({ title }: PropsType) {
   return (
-    <h1 className="text-8xl max-md:text-6xl font-semibold leading-snug">
+    <h1 className="text-8xl max-md:text-6xl max-[500px]:text-4xl font-semibold leading-snug">
       {title}
     </h1>
   );
