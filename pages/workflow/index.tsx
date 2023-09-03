ca85c2cc2d0d4185b2aae743c36c8ff07ab9a87b
Sergei Vorfolomeev <vorfo1897@gmail.com>
2023-09-04 00:42:50 +0200

    feat: add workflow page

---
+++ pages/workflow/index.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { WorkflowLayout } from "@/pages/workflow/workflow-layout";
+import { Navbar } from "@/components/navbar/navbar";
+import { motion } from "framer-motion";
+
+const PIPELINE = [
+  "Определение целей и задач клиента",
+  "Разработка концепции и сценария",
+  "Подбор локаций для съемки, актеров или моделей, необходимых оборудования",
+  "Подготовка технического оборудования и инструментов для съемки (камеры, микрофоны, стабилизаторы, освещение и т.д.)",
+  "Съемка",
+  "Монтаж видео: нарезка сцен, добавление музыки, звуковых эффектов, цветокоррекция и т.д.",
+  "Добавление спецэффектов, если необходимо",
+  "Создание и добавление заголовков, плашек или иной графики",
+  "Монтаж звуковой дорожки",
+  "Подготовка и оптимизация видео для экспорта: выбор формата и разрешения, сжатие, кодирование",
+  "Экспорт видео",
+  "Передача клиенту готового видео",
+];
+
+const Workflow = () => {
+  const animation = {
+    hidden: {
+      opacity: 0,
+      scale: 0.5,
+    },
+    visible: (custom: number) => ({
+      opacity: 1,
+      scale: 1,
+      transition: {
+        duration: 1,
+        delay: custom * 0.3,
+      },
+    }),
+  };
+
+  return (
+    <WorkflowLayout navbar={<Navbar />}>
+      {PIPELINE.map((el, index) => {
+        return (
+          <motion.div
+            key={index}
+            className="relative w-[300px] h-[130px] shadow-[20px_20px_50px_rgba(0,_0,_0,_0.5)] bg-white/10 overflow-hidden flex justify-center items-center rounded-2xl border-t-[1px] border-t-white/40 border-l-[1px] border-l-white/40 backdrop-blur-sm"
+            variants={animation}
+            initial={"hidden"}
+            animate={"visible"}
+            custom={index}
+            viewport={{ once: true }}
+          >
+            <div className="p-5 text-center duration-500">
+              <h2 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[200px] opacity-5 pointer-events-none ">
+                {index}
+              </h2>
+              <p>{el}</p>
+            </div>
+          </motion.div>
+        );
+      })}
+    </WorkflowLayout>
+  );
+};
+
+export default Workflow;
---
+++ pages/workflow/workflow-layout.tsx
@@ -0,0 +1,35 @@
+import React, { ReactNode } from "react";
+import { motion } from "framer-motion";
+
+type PropsType = {
+  navbar: ReactNode;
+  children: ReactNode;
+};
+
+export function WorkflowLayout({ navbar, children }: PropsType) {
+  return (
+    <motion.div className="h-screen bg-default relative">
+      {navbar}
+      <motion.div
+        className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-orange-500 to-violet-500"
+        initial={{ clipPath: "circle(10% at right 70%)" }}
+        animate={{ clipPath: "circle(45% at right 60%)" }}
+        transition={{ duration: 4 }}
+      />
+      {/*<motion.div*/}
+      {/*  className="absolute top-0 left-0 w-[150px] h-[150px] bg-gradient-to-b from-[#2196f3] to-[#e91e63]"*/}
+      {/*  initial={{ clipPath: "circle(20% at 10% 50%)" }}*/}
+      {/*  animate={{*/}
+      {/*    x: [-100, 300, 600, 900, 500, 200],*/}
+      {/*    y: [-100, 100, 400, 700, 1200, 900],*/}
+      {/*  }}*/}
+      {/*  transition={{ duration: 3 }}*/}
+      {/*/>*/}
+      <section className="flex items-center max-w-[1280px] 2xl:scale-150 2xl:top-[400px] mx-auto min-h-[80vh] pt-24">
+        <div className="relative flex flex-wrap max-lg:flex-col gap-10 justify-center items-center max-w-[1200px] mx-auto">
+          {children}
+        </div>
+      </section>
+    </motion.div>
+  );
+}
