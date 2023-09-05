c38ac95d2c096da3eaa1991b552c69b2e9b6cc86
Sergei Vorfolomeev <vorfo1897@gmail.com>
2023-09-05 15:37:12 +0200

    feat: animation with circles at the workflow page

--- components/workflow/workflow-layout.tsx
+++ components/workflow/workflow-layout.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from "react";
 import { motion } from "framer-motion";
+import { useWindowSize } from "@uidotdev/usehooks";
 
 type PropsType = {
   navbar: ReactNode;
@@ -7,31 +8,44 @@ type PropsType = {
 };
 
 export function WorkflowLayout({ navbar, children }: PropsType) {
+  const { width } = useWindowSize();
   return (
-    <motion.div className="h-screen bg-default relative">
+    <motion.div className="h-full bg-default relative">
       {navbar}
-      <motion.div
-        className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-orange-500 to-violet-500"
-        initial={{ clipPath: "circle(1% at right 1%)" }}
-        animate={{ clipPath: "circle(45% at right 0%)" }}
-        transition={{
-          duration: 4,
-          type: "spring",
-          mass: 2,
-          damping: 10,
-          stiffness: 150,
-        }}
-      />
-      {/*<motion.div*/}
-      {/*  className="absolute top-0 left-0 w-[150px] h-[150px] bg-gradient-to-b from-[#2196f3] to-[#e91e63]"*/}
-      {/*  initial={{ clipPath: "circle(20% at 10% 50%)" }}*/}
-      {/*  animate={{*/}
-      {/*    x: [-100, 300, 600, 900, 500, 200],*/}
-      {/*    y: [-100, 100, 400, 700, 1200, 900],*/}
-      {/*  }}*/}
-      {/*  transition={{ duration: 3 }}*/}
-      {/*/>*/}
-      <section className="flex items-center max-w-[1280px] 2xl:scale-150 mx-auto min-h-[80vh] pt-24">
+      <section className="flex items-center max-w-[1280px] 2xl:scale-150 mx-auto min-h-[80vh] py-24">
+        <motion.div
+          className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-orange-500 to-violet-500"
+          initial={{ clipPath: "circle(1% at right 1%)" }}
+          animate={
+            width && width > 640
+              ? { clipPath: "circle(45% at right 0%)" }
+              : { clipPath: "circle(20% at right 0%)" }
+          }
+          transition={{
+            duration: 4,
+            type: "spring",
+            mass: 2,
+            damping: 10,
+            stiffness: 50,
+          }}
+        />
+        <motion.div
+          className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-orange-500 to-violet-500"
+          initial={{ clipPath: "circle(1% at left 90%)" }}
+          animate={
+            width && width > 640
+              ? { clipPath: "circle(30% at left 90%)" }
+              : { clipPath: "circle(10% at left 90%)" }
+          }
+          transition={{
+            duration: 4,
+            type: "spring",
+            mass: 2,
+            damping: 10,
+            stiffness: 50,
+            delay: 0.3,
+          }}
+        />
         <div className="relative flex flex-wrap  gap-10 justify-center items-center max-w-[1200px] mx-auto">
           {children}
         </div>
